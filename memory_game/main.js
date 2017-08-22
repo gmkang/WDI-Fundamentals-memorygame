5f9aa0f29103bd92ca9f9d3e9375154e045c0760
Gina Kang <ginakang@Ginas-MacBook-Pro.local>
2017-08-21 23:05:46 -0400

    created game logic

---
+++ memory_game/main.js
@@ -0,0 +1,19 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	  alert("You found a match!");
+	} else {
+	  alert("Sorry, try again.");
+  	}
+  }
+
+function flipCard(cardId) {
+	console.log(`User flipped ${cards[cardId]}`);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+  flipCard(0);
+  flipCard(2);
+
